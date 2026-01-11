6444e35490b547826822f00efc4ceb3818a52ebc
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2026-01-11 13:19:32 +0530

    ll

--- start_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting VigilAI Backend Server...
-echo Backend will run on http://localhost:5000
-echo.
-python flask_api.py
--- start_frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting VigilAI Frontend...
-echo Frontend will run on http://localhost:5173
-echo.
-cd vigilai-ui
-npm run dev
